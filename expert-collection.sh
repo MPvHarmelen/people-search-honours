3098293f56ce1088c1da410664d37f1a69ab53ef
Verna Dankers <verna_dankers@hotmail.com>
2015-10-14 23:16:49 +0200

    Edited expert-collection.sh

--- expert-collection.sh
+++ expert-collection.sh
@@ -1,6 +1,5 @@
 make
-time ./word2vec -train expert-collection.txt -output vectors.bin -cbow 1 -size 200 -window 8 -negative 25 -hs 0 -sample 1e-4 -threads 20 -binary 1 -iter 15
-./distance vectors.bin
+time ../word2vec/word2vec -train ../expert-collection -output vectors.bin -cbow 1 -size 200 -window 8 -negative 25 -hs 0 -sample 1e-4 -threads 20 -binary 1 -iter 15
 
 # - desired vector dimensionality
 # - the size of the context window for either the Skip-Gram or the Continuous Bag-of-Words model
