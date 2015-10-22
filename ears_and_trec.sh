4c6b05a3768500c299d8a8799e334d9b77922b00
Martin van Harmelen <Martin@vanharmelen.com>
2015-10-23 00:22:44 +0200

    Removed old outputs
    I'm not adding the new ones because they are ~100 MB each.

--- ears_and_trec.sh
+++ ears_and_trec.sh
@@ -6,6 +6,9 @@ WORKING_DIR=$(pwd)
 # Convert input file to absolute path
 INPUT_FILE=$(readlink -m $1)
 
+# Create output directory if it doesn't exist
+mkdir -p output
+
 # Run ears
 cd ..
 ears-reloaded/ears ef $INPUT_FILE
