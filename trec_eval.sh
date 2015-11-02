5689fda8b2341dc21a34550197f2b92e1e03319d
Martin van Harmelen <Martin@vanharmelen.com>
2015-11-02 11:57:52 +0100

    Removed qrels from repo

--- trec_eval.sh
+++ trec_eval.sh
@@ -1,12 +1,13 @@
 ../trec_eval.9.0/trec_eval \
     -m all_trec \
     -q \
-    qrels-tu/GT1_self_selected_all_experts.qrel \
+    ../tu-expert-collection/qrels/expert_finding/GT1_self_selected_all_experts.qrel \
     $1 \
     > trec_out/$(expr $1 : '.*/\(.*\)\.out').gt1
+
 ../trec_eval.9.0/trec_eval \
     -m all_trec \
     -q \
-    qrels-tu/GT5_judged_system_generated_graded.qrel \
+    ../tu-expert-collection/qrels/expert_finding/GT5_judged_system_generated_graded.qrel \
     $1 \
     > trec_out/$(expr $1 : '.*/\(.*\)\.out').gt5
