98a6b48134b412d1b9742e3a8b2c90d95a5b59fe
Martin van Harmelen <Martin@vanharmelen.com>
2016-01-16 17:45:14 +0100

    Improved map reader

--- commandline/get_map.sh
+++ commandline/get_map.sh
@@ -4,17 +4,20 @@ SMOOTHING_METHODS="dirichlet"
 LANGUAGES="uk"
 MODELS="1 2"
 MEASURE="map"
+EXPANSION_METHODS="prf"
 
 for LANGUAGE in $LANGUAGES; do
 for SMOOTHING_METHOD in $SMOOTHING_METHODS; do
 for MODEL in $MODELS; do
+for EXPANSION_METHOD in $EXPANSION_METHODS; do
 for GT in $GTS; do
-    echo "$LANGUAGE $SMOOTHING_METHOD model $MODEL gt$GT"
-    FILES=$(ls -1v ../trec_out/ | grep "tu-$LANGUAGE-model$MODEL-$SMOOTHING_METHOD-.*\.gt$GT" | awk '{print "../trec_out/"$0}')
+    echo "$LANGUAGE $SMOOTHING_METHOD model $MODEL $EXPANSION_METHOD gt$GT"
+    FILES=$(ls -1v ../trec_out/ | grep -P "tu-$LANGUAGE-model$MODEL-$SMOOTHING_METHOD-$EXPANSION_METHOD-\d+\.gt$GT" | awk '{print "../trec_out/"$0}')
     # for FILE in $FILES; do
     #     echo $FILE
     # done
     cat $FILES | grep "^$MEASURE " | cut -f 3
+done
 done
 done
 done
